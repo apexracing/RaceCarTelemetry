77b197e7d839d1106c4d4958cc59071806b05c70
huntsman123 <hunt-man@163.com>
2021-12-24 16:08:39 +0800

    change track trigger

--- rct_simrace_tools/src/renderer/components/LapsPage.vue
+++ rct_simrace_tools/src/renderer/components/LapsPage.vue
@@ -1,6 +1,7 @@
 <template>
 	<div>
 		<div class="info">
+
 			<el-row type="flex" align="middle">
 				<el-col :span="12">
 					<el-descriptions>
@@ -22,12 +23,35 @@
 					</el-descriptions>
 				</el-col>
 				<el-col :span="12">
-					<div id="track_map" width="400" height="400"></div>
+					<div id="track_map">
+					</div>
 				</el-col>
 			</el-row>
 		</div>
-		<span>&nbsp;</span>
-		<el-table :data="lapsData" style="width: 100%" height="250" border stripe>
+		<div class="play">
+			<el-row>
+				<el-col :span="8">
+					<el-row align="middle" type="flex" justify="start">
+						<el-col :span="8">
+							<el-button type="primary" circle icon="el-icon-video-play"></el-button>
+						</el-col>
+						<el-col :span="4">
+							<el-button circle icon="el-icon-d-arrow-left"></el-button>
+						</el-col>
+						<el-col :span="4">
+							<el-button type="text" circle style="color:#303133;font-size: large;">0x</el-button>
+						</el-col>
+						<el-col :span="4">
+							<el-button circle icon="el-icon-d-arrow-right"></el-button>
+						</el-col>
+					</el-row>
+				</el-col>
+				<el-col :span="16">
+					<el-slider v-model="trackerPlayer"></el-slider>
+				</el-col>
+			</el-row>
+		</div>
+		<el-table :data="lapsData" style="width: 100%" height="200" border stripe class="table">
 			<el-table-column prop="lap" label="lap" width="80">
 			</el-table-column>
 			<el-table-column prop="topspeed" label="topspeed" width="180">
@@ -59,12 +83,15 @@
 		dialog
 	} = require('electron').remote;
 
-	import * as d3 from 'd3/dist/d3.min'
+	import * as d3 from 'd3/dist/d3'
 
 	export default {
 		name: 'laps-page',
 		data() {
 			return {
+				trackerPlayer: 0,
+				triggers: [],
+				trigger_id: 0,
 				vob_column: [],
 				vob_data: [],
 				realtime_show: true,
@@ -80,7 +107,7 @@
 				}]
 			}
 		},
-		mounted() {
+		created() {
 			switch (this.$route.params.deviceId) {
 				case 'ac':
 					{
@@ -134,7 +161,6 @@
 					}
 			}
 		},
-
 		methods: {
 			//分析vbo文件
 			process_vob(file, loading) {
@@ -166,8 +192,8 @@
 					}
 				});
 				objReadline.on('close', () => {
-					this.analyse_vob();
 					console.log("VOB文件分析完成")
+					this.render_track();
 					//console.log(this.vob_column);
 					//console.log(this.vob_data);
 					loading.close();
@@ -178,17 +204,17 @@
 			 * 算法1:取中间三分之一GPS平均坐标，与服务器赛道平均坐标对比，自动匹配赛道起点和分段
 			 * 算法2:服务器没有的赛道，取最高速度作为默认起点，用户可通过滑动条 修改“起点/终点”。
 			 */
-			analyse_vob() {
+			render_track() {
 				var margin = {
-						top: 10,
+						top: 20,
 						right: 20,
-						bottom: 10,
+						bottom: 20,
 						left: 20
 					},
-					width = 400 - margin.left - margin.right,
-					height = 400 - margin.top - margin.bottom;
+					width = 350 - margin.left - margin.right,
+					height = 350 - margin.top - margin.bottom;
 
-				var svg = d3.select("#track_map").append("svg")
+				var g = d3.select("#track_map").append("svg")
 					.attr("width", width + margin.left + margin.right)
 					.attr("height", height + margin.top + margin.bottom)
 					.append("g")
@@ -207,7 +233,7 @@
 					.range([height, 0])
 
 
-				svg.append("path")
+				var path = g.append("path")
 					.datum(this.vob_data)
 					.attr("fill", "none")
 					.attr("stroke", "green")
@@ -219,19 +245,74 @@
 						.y(function(d) {
 							return yscl(d.long);
 						}))
-
-
-				//this.$data.track_map[0].y = this.vob_data["long"]; //第一层地图数据，第二层赛道trigger
-				var max_speed = this.vob_data.reduce(function(prev, current) {
-					return (prev.velocity > current.velocity) ? prev : current
-				}) //returns object
-				console.log("最高速度:" + max_speed.velocity)
 				//计算赛道“起/终点”
 
+				var max_idx = d3.scan(this.vob_data, function(a, b) {
+					return b.velocity - a.velocity
+				});
+				var auto_trigger = this.vob_data[max_idx];
+				this.add_trigger_in_path([xscl(auto_trigger.lat),yscl(auto_trigger.long)]);
 				//计算单圈数据
+				path.on("click", (e, d) => {
+					var pointer = d3.pointer(e);
+					this.add_trigger_in_path(pointer);
 
+				});
 				//取最快圈做为赛道预览图
 
+			},
+			add_trigger_in_path(point){
+				var node = d3.select("path").node();
+				var pathLen = node.getTotalLength();
+				var distanceall = [];
+				//粗略查询
+				for (var i = 0; i < pathLen; i += 20) {
+					var dest = node.getPointAtLength(i);
+					var dx = dest.x - point[0];
+					var dy = dest.y - point[1];
+					var distance = dx * dx + dy * dy;
+					distanceall.push({
+						'distance': distance,
+						'idx': i
+					});
+				}
+				var clickIdx = distanceall[d3.minIndex(distanceall, d => d.distance)].idx;
+				//精细查找
+				distanceall = [];
+				for (var i = clickIdx; i < clickIdx + 20 && i < pathLen; i++) {
+					var dest = node.getPointAtLength(i);
+					var dx = dest.x - point[0];
+					var dy = dest.y - point[1];
+					var distance = dx * dx + dy * dy;
+					distanceall.push({
+						'distance': distance,
+						'idx': i
+					});
+				}
+				var clickIdx = distanceall[d3.minIndex(distanceall, d => d.distance)].idx;
+				var dest = node.getPointAtLength(clickIdx);
+				var near = node.getPointAtLength(clickIdx - 1);
+				var angle = Math.atan2(dest.y - near.y, dest.x - near.x) * 180 / Math.PI;
+				//V1中保存一个分割
+				this.triggers.shift();
+				this.triggers.push({
+					x: dest.x,
+					y: dest.y,
+					angle: angle,
+					id: this.trigger_id++
+				});
+				this.render_trigger();
+			},
+			render_trigger() {
+				var trigger = d3.select("svg").select("g").selectAll("line.track_trigger").data(this.triggers, (d) => d.id);
+				trigger.enter().append("line").attr(
+						"x1", (d) => d.x).attr("x2", (d) =>
+						d.x).attr(
+						"y1", (d) => d.y - 15).attr("y2", (d) => d.y + 15).attr("transform", (d) => "rotate(" + d.angle +
+						"," + d.x +
+						"," + d.y + ")")
+					.attr("class", "track_trigger");
+				trigger.exit().remove()
 			},
 			start_ac() {
 				const client = new ACRemoteTelemetryClient("localhost");
@@ -263,6 +344,24 @@
 <style>
 	.info {
 		box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+
 		padding: 15px;
+		margin-bottom: 10px;
+	}
+
+	.play {
+		border: 1px solid #DCDFE6;
+		border-radius: 0px;
+		padding: 15px;
+		margin-bottom: 10px;
+	}
+
+	.table {
+		box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+	}
+
+	svg .track_trigger {
+		stroke: #000000;
+		stroke-width: 2;
 	}
 </style>
